97f5a3809f63104b82165934ee2c83fe54ce8f12
Ipsita Samal <ipsitabits@gmail.com>
2016-06-24 21:31:37 +0530

    Update MetadataParser.java
    added code to use odml-java-lib, fixed bug regarding recursive sections, added support for gui-namespaces

--- src/MetadataParser.java
+++ src/MetadataParser.java
@@ -1,13 +1,16 @@
+import odml.core.Reader;
 import org.g_node.nix.Block;
 import org.g_node.nix.Property;
 import org.g_node.nix.Section;
 import org.g_node.nix.Value;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Vector;
+
 
 /**
  * Created by ipsita on 23/6/16.
@@ -24,27 +27,18 @@ public class MetadataParser {
         this.metadataFile = "metadata.xml";
     }
 
-    /**
-     *
-     * The method takes in an input file and creates a DOM document which
-     * can be parsed like a tree using XML parser.
-     *
-     * @param inputFile : The file used to create a DOM parsing tree of the same
-     * @return Document : a DOM Model document is returned which can then be parsed
-     */
-    private Document initializeDOMParser(java.io.File inputFile){
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        Document doc = null;
-        DocumentBuilder dBuilder = null;
+
+    private odml.core.Section initializeODMLReader(){
+        Reader reader = new Reader();
+        odml.core.Section rootSection=new odml.core.Section();
+        InputStream inputstream;
         try {
-            dBuilder = dbFactory.newDocumentBuilder();
-            doc = dBuilder.parse(inputFile);
+            inputstream = new FileInputStream(metadataFile);
+            rootSection = reader.load(inputstream, true);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        doc.getDocumentElement().normalize();
-        return doc;
-
+        return rootSection;
     }
 
     /**
@@ -57,112 +51,150 @@ public class MetadataParser {
      * @param file : HDF5 (Specifically nix) file in which this metadata will be written
      */
     public void setMetadata(String metadataFile, Block block, org.g_node.nix.File file) {
+
         java.io.File inputFile = new java.io.File(metadataFile);
 
         try {
-            Document doc = initializeDOMParser(inputFile);
-            NodeList versionList = doc.getElementsByTagName("version");
-            String version = versionList.item(0).getTextContent();
-
-            NodeList dateList = doc.getElementsByTagName("date");
-            String date = dateList.item(0).getTextContent();
+            odml.core.Section rootSection = initializeODMLReader();
+            String version = rootSection.getDocumentVersion();
+            String date = rootSection.getDocumentDate().toString();
 
             System.out.println("version : " + version);
             System.out.println("date : " + date);
 
             // create section and add a property
-            Section sectionMetadata = file.createSection("metadataSection", "metadata");
-            block.setMetadata(sectionMetadata);
+            Section rootSectionMetadata = file.createSection("metadataSection", "metadata");
+            block.setMetadata(rootSectionMetadata);
 
             //for parsing
-            NodeList sectionList = doc.getElementsByTagName("section");
+            Vector<odml.core.Section> sectionVector = rootSection.getSections();
 
-            for (int temp = 0; temp < sectionList.getLength(); temp++) {
-
-                Node thisSection = sectionList.item(temp);
-                System.out.println("temp : " + temp + " | Current section : " + thisSection.getNodeName());
-
-                NodeList sectionChildren = thisSection.getChildNodes();
-
-                String typeOfSection = "";
-                String nameOfSection = "";
-
-                //do for each child of this section (for type and name)
-                for (int i = 0; i < sectionChildren.getLength(); i++) {
-
-                    Node sectionChild = sectionChildren.item(i);
-
-                    if (sectionChild.getNodeName().equalsIgnoreCase("type")) {
-                        //assign type to the section
-                        typeOfSection = sectionChild.getTextContent();
-                    } else if (sectionChild.getNodeName().equalsIgnoreCase("name")) {
-                        //assign name to the section
-                        nameOfSection = sectionChild.getTextContent();
-                    }
-                }
-
-                // hdf5 creating subsection of metadata section (root)
-                Section secChild = sectionMetadata.createSection(nameOfSection, typeOfSection);
-
-                //for each child of section (for property)
-                for (int i = 0; i < sectionChildren.getLength(); i++) {
-                    Node sectionChild = sectionChildren.item(i);
-
-                    if (sectionChild.getNodeName().equalsIgnoreCase("property")) {
-                        String nameOfProperty = "";
-                        Value valueOfProperty = new Value("");  // default value since creating property requires a value and some properties don't have one
-
-                        NodeList propertyChildren = sectionChild.getChildNodes();
-
-                        for (int j = 0; j < propertyChildren.getLength(); j++) {
-                            Node propertyChild = propertyChildren.item(j);
-
-                            if (propertyChild.getNodeName().equalsIgnoreCase("name")) {
-                                nameOfProperty = propertyChild.getTextContent();
-                            }
-                            else if (propertyChild.getNodeName().equalsIgnoreCase("value")) {
-
-                                NodeList valueDetails = propertyChild.getChildNodes();
-                                String value = valueDetails.item(0).getTextContent().split("\n")[1].trim();
-                                String valueType = valueDetails.item(1).getTextContent();
-                                System.out.println("Value = " + value + ", valueType = " + valueType);
-                                valueOfProperty = new Value();
-                                switch (valueType) {
-                                    case "datetime":
-                                        valueOfProperty.setString(value);
-                                        break;
-                                    case "int":
-                                        valueOfProperty.setInt(Integer.parseInt(value));
-                                        break;
-                                    case "float":
-                                        valueOfProperty.setDouble(Double.parseDouble(value));
-                                        break;
-                                    case "boolean":
-                                        valueOfProperty.setBoolean(Boolean.parseBoolean(value));
-                                        break;
-                                    case "string":
-                                        valueOfProperty.setString(value);
-                                        break;
-                                    case "long":
-                                        valueOfProperty.setLong(Long.parseLong(value));
-                                        break;
-                                    default:
-                                        System.out.println("ERROR. Some wrong valueType. valueType : " + valueType);
-                                }
-                            }
-                            else {
-
-                            }
-                        }
-
-                        Property prop = secChild.createProperty(nameOfProperty, valueOfProperty);
-                    }
-                }
+            if(sectionVector.size()>0){
+                setSection(rootSectionMetadata, sectionVector);
             }
         } catch (Exception e) {
+            System.out.println("Exception!!!!");
 
         }
 
+    }
+
+    public void setSection(Section parentSection, Vector<odml.core.Section> sectionVector){
+        for (int temp = 0; temp < sectionVector.size(); temp++)
+        {
+
+            odml.core.Section thisSection = sectionVector.get(temp);
+            System.out.println("temp : " + temp + " | Current section : " + thisSection.getName());
+
+            String typeOfSection = "";
+            String nameOfSection = "";
+            Vector<odml.core.Property> propertiesList = new Vector<>();
+
+            if(thisSection.getType()!=null){
+                typeOfSection = thisSection.getType();
+            }
+
+            if(thisSection.getName()!=null){
+                nameOfSection = thisSection.getName();
+            }
+            // hdf5 creating subsection of metadata section (root)
+            Section secChild = parentSection.createSection(nameOfSection, typeOfSection);
+
+            //System.out.println("This section's sections: "+ thisSection.getSections().size());
+
+            //this is the recursive function since a section may have a section inside it, and so on.
+            if(thisSection.getSections()!=null && thisSection.getSections().size()>0){
+                setSection(secChild, thisSection.getSections());
+            }
+
+            if(thisSection.getProperties()!=null){
+                propertiesList = thisSection.getProperties();
+            }
+            setProperties(secChild, propertiesList);
+        }
+
+
+    }
+
+    public void setProperties(Section parentSec, Vector<odml.core.Property> propertiesList){
+        //for each child of section (for property)
+        if(propertiesList!=null && !propertiesList.isEmpty()){
+            String nameOfProperty = "";
+            Value valueOfProperty = new Value("");
+            for(int tempProperty = 0; tempProperty < propertiesList.size(); tempProperty++) {
+                odml.core.Property thisProperty = propertiesList.get(tempProperty);
+                System.out.println("    tempProperty : " + tempProperty + " | Current property : " + thisProperty.getName());
+                if(thisProperty.getName()!=null){
+                    nameOfProperty = thisProperty.getName();
+                }
+
+
+                //System.out.println("-----------type: "+ thisProperty.getWholeValue().getMap().get("type") + " | value " + thisProperty.getValue());
+                if(thisProperty.valueCount()>0){
+                    odml.core.Value wholeValue = thisProperty.getWholeValue();
+                    String value = thisProperty.getValue().toString();
+                    String valueType= wholeValue.getMap().get("type").toString();
+                    switch (valueType) {
+                        case "datetime":
+                            valueOfProperty.setString(value);
+                            break;
+                        case "int":
+                            valueOfProperty.setInt(Integer.parseInt(value));
+                            break;
+                        case "float":
+                            valueOfProperty.setDouble(Double.parseDouble(value));
+                            break;
+                        case "boolean":
+                            valueOfProperty.setBoolean(Boolean.parseBoolean(value));
+                            break;
+                        case "string":
+                            valueOfProperty.setString(value);
+                            break;
+                        case "long":
+                            valueOfProperty.setLong(Long.parseLong(value));
+                            break;
+                        default:
+                            System.out.println("ERROR. Some wrong valueType. valueType : " + valueType);
+                    }
+                }
+                processGUINamespaces(parentSec, thisProperty);
+            }
+            Property prop = parentSec.createProperty(nameOfProperty, valueOfProperty);
+        }
+    }
+
+//    public void setGUINamespace(String nameOfGUINamespace, Boolean valueOfGUINamespace, Section guiSection){
+//        Value gui_required_value = new Value(valueOfGUINamespace);
+//        guiSection.createProperty(nameOfGUINamespace, gui_required_value);
+//    }
+
+    public void processGUINamespaces(Section parentSec, odml.core.Property property){
+        Section guiSection = parentSec.createSection(property.getName(), "GUI:Namespace");
+        Boolean gui_required;
+        Boolean gui_editable;
+        int gui_order;
+        if(property.getGuiHelper().getRequired()!=null) {
+            gui_required = property.getGuiHelper().getRequired();
+            Value gui_required_value = new Value(gui_required);
+            guiSection.createProperty("gui_required", gui_required_value);
+
+        }
+        if(property.getGuiHelper().getEditable()!=null) {
+            gui_editable= property.getGuiHelper().getEditable();
+            Value gui_editable_value = new Value(gui_editable);
+            guiSection.createProperty("gui_empty", gui_editable_value);
+        }
+
+        if(property.getGuiHelper().getRequired()!=null) {
+            gui_order = property.getGuiHelper().getOrder();
+            Value gui_order_value = new Value(gui_order);
+            guiSection.createProperty("gui_empty", gui_order_value);
+        }
+
+//        List list = property.getGuiHelper().getGUINamespaceTags();
+//        for(int i=0; i<list.size(); i++){
+//            System.out.println("->"+list.get(i));
+//        }
     }
 
 }
